7e9e14381237d2b15fa7cf369bce486cb7ffa3fe
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-08-06 13:56:01 +0000

    Deploy irvingsl/tDCS to github.com/irvingsl/tDCS.git:gh-pages

---
+++ search/pages_0.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['tdcs_20_20_5b_21_5bbuild_20status_5d_28https_3a_2f_2ftravis_2dci_2ecom_2firvingsl_2ftdcs_2esvg_3ftoken_3dyznhixteqvzmiuiog4av_26branch_3ddevelop_29_5d_28https_3a_2f_2ftravis_2dci_2ecom_2firvingsl_2ftdcs_29',['tDCS  [![Build Status](https://travis-ci.com/irvingsl/tDCS.svg?token=yzNhiXtEqvZmiUioG4av&amp;branch=develop)](https://travis-ci.com/irvingsl/tDCS)',['../index.html',1,'']]]
+];
---
+++ search/searchdata.js
@@ -0,0 +1,21 @@
+var indexSectionsWithContent =
+{
+  0: "mt",
+  1: "m",
+  2: "t"
+};
+
+var indexSectionNames =
+{
+  0: "all",
+  1: "classes",
+  2: "pages"
+};
+
+var indexSectionLabels =
+{
+  0: "Todos",
+  1: "Classes",
+  2: "Páginas"
+};
+
